c865ad3430aba6e4eb6f3c1508a2925623b48764
Daniel Eiro <daniel.eiro.01@gmail.com>
2024-02-29 21:26:53 -0300

    feat: adding message validation

--- src/receiver.ts
+++ src/receiver.ts
@@ -1,23 +1,38 @@
 import { SQSEvent } from 'aws-lambda';
 import { EmailUseCase } from './usecases/emailUseCase'
+import { assertArgumentIsValidEmail, assertArgumentIsValidStatus } from "@/utils/assertionConcern";
 
 const consumeSqsMessage = async (event: SQSEvent) => {
     try {
         const emailUseCase = new EmailUseCase();
         for (const record of event.Records) {
-            // Process each SQS message
-            const messageBody = JSON.parse(record.body);
+            const messageBody = JSON.parse(record.body || "{}");
             console.log('Received message:', messageBody);
-            const mailMessage = { 
-                recipient: "jane@soat.com.br",
-                name: "Jane Doe",
-                status: "recebido"
+            console.log(messageBody.clienteNome)
+            
+            const messageEmail = messageBody.clienteEmail.props.address
+            const messageNome = messageBody.clienteNome
+            const messageStatus = messageBody.statusPedido
+            const messagePedido = messageBody.messageStatus
+            const isValidEmail = assertArgumentIsValidEmail(messageEmail)
+            const isValidStatus = assertArgumentIsValidStatus(messageStatus)
+
+            if (!isValidEmail) {
+                console.error("Skipping message: Incorrect e-mail format, ex: abx@xyz.com");
+                continue;
+            }
+            if (!isValidStatus) {
+                console.error("Skipping message: Incorrect status value");
+                continue;
+            }
+            const mailMessage = {
+                id: messagePedido, 
+                recipient: messageEmail,
+                name: messageNome,
+                status: messageStatus
             }
             await emailUseCase.sendEmail(mailMessage)
         }
-        return { 
-            statusCode: 200, body: 'Messages processed successfully' 
-        };
     } catch (error) {
         console.error('Error processing messages:', error);
         throw error;
